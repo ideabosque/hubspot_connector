a493fb74a9ae32e69712cac4f1738cc33556d1e2
Jeffrey Wang <jefferyw@ingredientsonline.com>
2022-08-13 12:15:07 +0800

    update

--- hubspot_connector/__init__.py
+++ hubspot_connector/__init__.py
@@ -4,7 +4,3 @@
 __all__ = ["hubspotconnector"]
 
 from .hubspotconnector import HubspotConnector
-from .api.contacts import Contacts
-from .api.companies import Companies
-from .api.deals import Deals
-from .api.products import Products
---
+++ hubspot_connector/api/__init__.py
@@ -0,0 +1,4 @@
+from .contacts import Contacts
+from .companies import Companies
+from .deals import Deals
+from .products import Products
